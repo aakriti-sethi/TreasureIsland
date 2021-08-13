fbf2bced2bf0c07371d52a69232da8f9fc70327d
Aakriti <github.com/aakriti-sethi>
2021-08-13 21:50:51 +0530

    Initial Commit

---
+++ TreasureIslandGame.py
@@ -0,0 +1,50 @@
+
+print('''
+ _                                     _     _                 _ 
+| |                                   (_)   | |               | |
+| |_ _ __ ___  __ _ ___ _   _ _ __ ___ _ ___| | __ _ _ __   __| |
+| __| '__/ _ \/ _` / __| | | | '__/ _ \ / __| |/ _` | '_ \ / _` |
+| |_| | |  __/ (_| \__ \ |_| | | |  __/ \__ \ | (_| | | | | (_| |
+ \__|_|  \___|\__,_|___/\__,_|_|  \___|_|___/_|\__,_|_| |_|\__,_|
+''')
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/[TomekK]
+*******************************************************************************
+''')
+print("welcome to the treasure island.")
+print("your mission is to find the treasure. all the best!")
+
+road = input("you are at a crossroad. where do you want to go? type 'left' or 'right'\n").lower()
+
+if road == "right":
+    print("you got hit by a car. game over.")
+else:
+    lake = input("you come to a lake. there is an island in the middle of the lake. type 'wait' to wait for the boat or type 'swim' to swim across.\n").lower()
+    if lake == "swim":
+        print("you drowned in the lake. game over.")
+    else:
+        door = input("you arrive at the island unharmed. there is a house with 3 doors red, yellow and blue. which color would you like to choose?\n").lower()
+        if door == "red":
+            print("congratulations! you found the treasure.")
+        elif door == "yellow":
+            print("you enter a room with fire. you burnt yourself. game over.")
+        else:
+            print("you enter a room of ghosts. they killed you. game over.")
